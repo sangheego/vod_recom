86bd4acdf0cdd8d14662ff6ff6939b9a8a9b1ed7
sangheego <gohc5004@naver.com>
2023-10-17 17:16:23 +0900

    '1017_success_error'

--- account/views.py
+++ account/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render,redirect
 from django.contrib import auth
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.urls import reverse
 
 # Create your views here.
-def login(request):
+def custom_login(request):
     if request.method=='POST':
         username=request.POST['username']
         password=request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('success')
+            return redirect('success.html')
         else:
             messages.error(request, '올바른 아이디가 아닙니다.')
     return render(request, 'login.html')
 
-def logout(request):
+def custom_logout(request):
     auth.logout(request)
     return redirect('home')
+
+def success(request):
+    return render(request, 'success.html')
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from account import views as account_views
+from account.views import success, custom_login, custom_logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', account_views.login,name='login'),
-    path('logout/', account_views.logout, name='logout'),
+    path('login/', custom_login, name='login'),
+    path('logout/', custom_logout, name='logout'),
+    path('success/', success, name='success'),
 ]
